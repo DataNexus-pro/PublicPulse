4d10e12355d995e09c080f859e603b74dd7346cd
DataNexus-pro <n211104@rguktn.ac.in>
2025-04-24 11:18:18 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,21 @@
-// Replace with your Firebase config from the Firebase Console
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
+  apiKey: "AIzaSyC6s53L0gqWSJzuKLdTodyxMbNeXjEg_B4",
+  authDomain: "datanexus-pro.firebaseapp.com",
+  projectId: "datanexus-pro",
+  storageBucket: "datanexus-pro.firebasestorage.app",
+  messagingSenderId: "919261212964",
+  appId: "1:919261212964:web:6a1e29e2015302714f43b1",
+  measurementId: "G-0WY5GGVKQ1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
